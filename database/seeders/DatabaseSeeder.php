8db3229b1fc7e66ab673d2d67b3bb1d8f22cc562
Suyatno <suyatnotkjc@gmail.com>
2024-01-03 14:50:23 +0700

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run()
+    {
+        $faker = Faker::create('id_ID');
+        for ($i = 0; $i < 10; $i++) {
+            DB::table('users')->insert([
+                //'id' => $faker->id,
+                'name' => $faker->name,
+                'email'=> $faker->email
+                ]);
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
+}
+}
+?>
